90c0b62d743ae28adb944defa4b8a44fcdfa4aef
Davron <yul.davron.93@gmail.com>
2022-12-14 21:18:34 +0500

    dfsd

--- index.js
+++ index.js
@@ -135,7 +135,26 @@ app.post("/api/sendOrderToCourier", async (req, res) => {
           ],
         },
       });
-
+      console.log(
+        JSON.stringify({
+          inline_keyboard: [
+            [
+              {
+                text: "Комментарии",
+                web_app: {
+                  url: `${process.env.WEB_LINK}/comments/?order_id=${order.id}&sign=${sign}`,
+                },
+              },
+            ],
+            [
+              {
+                text: "Подтвердить заказ",
+                callback_data: `confirmOrder/${order.id}/${sign}`,
+              },
+            ],
+          ],
+        })
+      );
       result.push({
         chatId,
         messageId: message_id,
